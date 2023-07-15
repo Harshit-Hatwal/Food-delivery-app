9ef4f8ef8b1c95361f8405db7a2f36fb37998fd0
Harshit-Hatwal <harshithatwal@gmail.com>
2023-07-15 14:35:51 +0530

    navbarmaking

--- food-delivery/src/App.js
+++ food-delivery/src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+
+import "./App.css";
+//  app.js mein navbar .js use karna ha toh 
+ // components and app.js same folder mein ha toh ./components
+ import Navbar from "./components/Navbar/Navbar.js";
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  // writing <Navbar is equivalent to writinng <Navbar> </Navbar> 
+    return <div > 
+      <Navbar />        
+      
+     
     </div>
-  );
+  
 }
 
 export default App;
---
+++ food-delivery/src/components/Navbar/Navbar.js
@@ -0,0 +1,65 @@
+// making the functional component .
+
+import React from "react";
+// importing the css
+import "./Navbar.css";
+
+
+
+function Navbar() {
+    return (    // componentname-classname this is the naming convention which 
+    //       // i will use in this project . 
+      <div className="navbar-container">
+      
+        {/* Logo  */}
+        <div>
+          <img
+            className="navbar-logo"
+            src="https://b.zmtcdn.com/web_assets/b40b97e677bc7b2ca77c58c61db266fe1603954218.png"
+            alt="Logo"
+          />
+        </div>
+        {/* Search  */}
+  <div>
+
+  </div>
+
+<div>
+
+  
+</div>
+
+
+        <div>
+
+    
+
+
+
+        </div>
+
+  
+
+
+      </div>
+    );
+  }
+  
+// function Navbar()
+// {
+//       // componentname-classname this is the naming convention which 
+//       // i will use in this project . 
+//     return <div className="navbar-container"> 
+
+//     <div >
+//        <img src=" "
+
+//         </img>
+//     </div>
+
+//     </div>;
+   
+
+// }
+
+export default Navbar;
